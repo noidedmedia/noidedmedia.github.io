5f146d06adbec077f6045a744c72d603d2f46fa1
Anthony Super <anthony@noided.media>
2017-03-01 12:38:07 -0700

    Some more cool animations

---
+++ js/make.js
@@ -0,0 +1,23 @@
+function startRoll(ary, idx) {
+  var quote = ary[idx % ary.length];
+  var elem = document.getElementById("rolling-content");
+  elem.style.opacity = 0;
+  elem.style.transform = "translateY(200%) rotateX(90deg)";
+  window.setTimeout(function() {
+    elem.style.opacity = 1;
+    elem.innerHTML = quote;
+    elem.style.transform = "translateY(0%)";
+    // wait 1.5 seconds
+    window.setTimeout(function(){
+      elem.style.transform = "translateY(-200%) rotateX(-90deg)";
+      window.setTimeout(startRoll.bind(null, ary, idx + 1), 500);
+    }, 5000)
+  }, 500);
+}
+
+
+fetch("/make.json").then(function(response) {
+  return response.json();
+}).then(function(resp) {
+  startRoll(resp, 1);
+});
